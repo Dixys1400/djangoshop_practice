c3b3a6751e3e2a37fa85d18e1cb3d1fba74611f6
Dixys1400 <dimamaper@gmail.com>
2025-07-26 20:24:53 +0200

    Initial commit

---
+++ myshop/store/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
+    path('category/<int:pk>/', views.category_products, name='category_products'),
+]
---
+++ myshop/store/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product, Category
+from django.core.paginator import Paginator
+
+
+def home(request):
+    products = Product.objects.all()
+    paginator = Paginator(products, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    categories = Category.objects.all()
+    return render(request, 'home.html', {'page_obj': page_obj, 'categories': categories})
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product_detail.html', {'product': product})
+
+
+def add_product(request):
+    categories = Category.objects.all()
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        price = request.POST.get('price')
+        category_id = request.POST.get('category')
+        category = get_object_or_404(Category, pk=category_id)
+        Product.objects.create(name=name, description=description, price=price, category=category)
+        return redirect('home')
+    return render(request, 'add_product.html', {'categories': categories})
+
+
+def category_products(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    products = category.products.all()
+    return render(request, 'category_products.html', {'category': category, 'products': products})
